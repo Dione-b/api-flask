804a325052b29859cf2568f1f5f86d9040506ffb
Dione-b <dionenassau321@gmail.com>
2024-02-24 20:39:12 -0300

    fix: api online

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def upload_image():
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
